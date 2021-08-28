7cdc2f60948557e63ff64c2873090632c62bab57
MicahGold <89551291+MicahGold@users.noreply.github.com>
2021-08-28 11:45:55 -0700

    Final Commit

--- src/ATM.java
+++ src/ATM.java
@@ -2,11 +2,11 @@ import java.util.*;
 public class ATM {
 	public HashMap <String, Integer> accounts = new HashMap <String, Integer> (); 
 	
-	private void deposit (String bankAccountID, int amount)
+	public void deposit (String bankAccountID, int amount)
 	{
 		if(accounts.putIfAbsent(bankAccountID, amount) != null)
 		{
 		accounts.replace(bankAccountID, accounts.get(bankAccountID)+amount); 
 	}
-}
+	}
 }
